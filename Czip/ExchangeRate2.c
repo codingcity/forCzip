7dd960066a3903cd553e833d6b9508f64e643916
cdct <codingcity.net@gmail.com>
2018-06-28 18:06:49 +0900

    First

--- Czip/ExchangeRate2.c
+++ Czip/ExchangeRate2.c
@@ -6,22 +6,30 @@ int main(void)
     int dollarExchangeRate = 1200;
     int howMuchDollar = 0;
     int krw;
-    char nationName;
 
     printf("Welcome!\n");
 
     printf("plz, input the dollarExchangeRate:");
     scanf(" %d",&dollarExchangeRate);
 
-    scanf(" %c",&nationName);
+    if( dollarExchangeRate > 2000 ){
+        printf("check the dollarExchangeRate!\n");
+    }
 
     printf("plz , input the dollar : ");
 
-    scanf(" %d",&howMuchDollar);
+    scanf("%d",&howMuchDollar);
+
+    if( howMuchDollar > 500){
+        printf("You have to declare");
+    }else{
+        printf("You don't have to declare");
+    }
+
 
     krw = howMuchDollar * dollarExchangeRate;
 
-    printf("%d($$$) -> %d(krw)?",howMuchDollar,krw);
+    printf("%d($$$) -> %d(krw)",howMuchDollar,krw);
 
     return 0;
 

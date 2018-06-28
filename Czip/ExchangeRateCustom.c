2b06cc84cbd8fde228e3ad96f8db6913a7d4376c
cdct <codingcity.net@gmail.com>
2018-06-28 16:56:28 +0900

    Custom

---
+++ Czip/ExchangeRateCustom.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+    int dollarExchangeRate = 1200;
+    int howMuchDollar = 0;
+    int krw;
+
+    printf("Welcome!\n");
+
+    printf("plz, input the dollarExchangeRate:");
+    scanf(" %d",&dollarExchangeRate);
+
+
+
+    printf("plz , input the dollar : ");
+
+    scanf("%d",&howMuchDollar);
+
+    krw = howMuchDollar * dollarExchangeRate;
+
+    printf("%d($$$) -> %d(krw)",howMuchDollar,krw);
+
+    return 0;
+
+}
